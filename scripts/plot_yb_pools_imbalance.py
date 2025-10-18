01db277e9f2bb5b5b1836ce44a6f98d6d2b6a48e
Michael Egorov <michwill@gmail.com>
2025-10-19 00:34:19 +0200

    Plot imbalances

---
+++ scripts/plot_yb_pools_imbalance.py
@@ -0,0 +1,58 @@
+import pylab
+import numpy as np
+from brownie import multicall
+from collections import defaultdict
+from datetime import datetime
+from brownie import Contract, config
+from brownie import web3
+
+config['autofetch_sources'] = True
+
+FACTORY = "0x370a449FeBb9411c95bf897021377fe0B7D100c0"
+START_BLOCK = 23434125
+N_POINTS = 1000
+
+
+def main():
+    mc = Contract("0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696")
+
+    factory = Contract(FACTORY)
+    n = factory.market_count()
+    markets = [factory.markets(i) for i in range(n)]
+    lts = [Contract(m[3]) for m in markets]
+    amms = [Contract(m[2]) for m in markets]
+    cryptopools = [Contract(amm.COLLATERAL()) for amm in amms]
+    collateral_decimals = [Contract(lt.ASSET_TOKEN()).decimals() for lt in lts]
+
+    current_block = web3.eth.block_number
+    blocks = [int(b) for b in np.linspace(START_BLOCK, current_block, N_POINTS)]
+
+    times = []
+    imbalances = defaultdict(list)
+
+    for b in blocks:
+        params = defaultdict(dict)
+        with multicall(address=mc.address, block_identifier=b):
+            t = mc.getCurrentBlockTimestamp()
+            for i in range(n):
+                params[i]['b0'] = cryptopools[i].balances(0)
+                params[i]['b1'] = cryptopools[i].balances(1)
+                params[i]['p_o'] = cryptopools[i].price_oracle()
+
+        t = datetime.fromtimestamp(t)
+        print(t)
+        times.append(t)
+
+        for i in range(n):
+            imbalance = params[i]['b0'] / (params[i]['b1'] * 10**(18 - collateral_decimals[i]) * params[i]['p_o'] / 1e18 + params[i]['b0'])
+            imbalances[i].append(imbalance)
+
+    for i in range(n):
+        pylab.plot(times, np.array(imbalances[i]) * 100, label=lts[i].symbol())
+
+    pylab.title('YB cryptopools imbalance in volatility')
+    pylab.ylabel('Fraction of pool in crvUSD [%]')
+    pylab.xticks(rotation=45, ha='right')
+    pylab.legend()
+    pylab.tight_layout()
+    pylab.show()
