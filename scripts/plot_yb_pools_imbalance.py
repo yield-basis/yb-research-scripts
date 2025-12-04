282e9f47c8b7be2f0fe3269ae28935f10f609aeb
Michael Egorov <michwill@gmail.com>
2025-12-04 17:43:01 +0100

    Pool imbalance

--- scripts/plot_yb_pools_imbalance.py
+++ scripts/plot_yb_pools_imbalance.py
@@ -17,7 +17,7 @@ def main():
     mc = Contract("0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696")
 
     factory = Contract(FACTORY)
-    n = factory.market_count()
+    n = 3
     markets = [factory.markets(i) for i in range(n)]
     lts = [Contract(m[3]) for m in markets]
     amms = [Contract(m[2]) for m in markets]
@@ -29,7 +29,7 @@ def main():
 
     times = []
     imbalances = defaultdict(list)
-    debts = defaultdict(list)
+    # debts = defaultdict(list)
 
     for b in blocks:
         params = defaultdict(dict)
@@ -39,7 +39,7 @@ def main():
                 params[i]['b0'] = cryptopools[i].balances(0)
                 params[i]['b1'] = cryptopools[i].balances(1)
                 params[i]['p_o'] = cryptopools[i].price_oracle()
-                params[i]['amm_debt'] = amms[i].get_debt()
+                # params[i]['amm_debt'] = amms[i].get_debt()
 
         t = datetime.fromtimestamp(t)
         print(t)
@@ -49,10 +49,12 @@ def main():
             pool_value = params[i]['b1'] * 10**(18 - collateral_decimals[i]) * params[i]['p_o'] / 1e18 + params[i]['b0']
             imbalance = params[i]['b0'] / pool_value
             imbalances[i].append(imbalance)
-            debts[i].append(params[i]['amm_debt'] / pool_value)
+            # debts[i].append(params[i]['amm_debt'] / pool_value)
+
+    colors = ["orange", "blue", "gray"]
 
     for i in range(n):
-        pylab.plot(times, np.array(imbalances[i]) * 100, label=lts[i].symbol())
+        pylab.plot(times, np.array(imbalances[i]) * 100, label=lts[i].symbol(), c=colors[i])
         # pylab.plot(times, np.array(debts[i]) * 100, '--', c=c)
 
     pylab.title('YB cryptopools imbalance in volatility')
