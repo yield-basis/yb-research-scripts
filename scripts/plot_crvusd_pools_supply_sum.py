1413405b29d2403466d291d1ea2b7ff15e494582
Michael Egorov <michwill@gmail.com>
2025-12-05 12:27:52 +0100

    Sum plots

---
+++ scripts/plot_crvusd_pools_supply_sum.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+
+from brownie import multicall
+from datetime import datetime
+from brownie import Contract, config
+from brownie import web3
+
+config['autofetch_sources'] = True
+
+
+MULTICALL = "0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696"
+START_BLOCK = 23575554
+N_POINTS = 500
+
+POOLS = {
+    'usdc': "0x4DEcE678ceceb27446b35C672dC7d61F30bAD69E",
+    'usdt': "0x390f3595bCa2Df7d23783dFd126427CCeb997BF4",
+    'frxusd': "0x13e12BB0E6A2f1A3d6901a59a9d585e89A6243e1",
+    'pyusd': "0x625E92624Bc2D88619ACCc1788365A69767f6200"
+}
+GAUGES = {
+    'usdc': "0x95f00391cB5EebCd190EB58728B4CE23DbFa6ac1",
+    'usdt': "0x4e6bB6B7447B7B2Aa268C16AB87F4Bb48BF57939",
+    'frxusd': "0x22804B0F6bE741a9Fa1BbaEcDD6c8D4116E96944",
+    'pyusd': "0xf69Fb60B79E463384b40dbFDFB633AB5a863C9A2"
+}
+
+
+def main():
+    mc = Contract(MULTICALL)
+
+    pools = {name: Contract(addr) for name, addr in POOLS.items()}
+    gauges = {name: Contract(addr) for name, addr in GAUGES.items()}
+
+    end_block = web3.eth.block_number
+    blocks = list(range(START_BLOCK, end_block, (end_block - START_BLOCK) // N_POINTS))
+
+    all_supplies = []
+    times = []
+    all_gauge_balances = []
+
+    for block in blocks:
+        supplies = {}
+        gauge_balances = {}
+        vprices = {}
+
+        with multicall(address=MULTICALL, block_identifier=block):
+            time = mc.getCurrentBlockTimestamp()
+            for name in pools.keys():
+                pool = pools[name]
+                gauge = gauges[name]
+                supplies[name] = pool.totalSupply()
+                gauge_balances[name] = pool.balanceOf(gauge.address)
+                vprices[name] = pool.get_virtual_price()
+
+        time = datetime.fromtimestamp(time)
+        print(time, block)
+
+        s = 0
+        g = 0
+        for name in pools.keys():
+            supply = supplies[name] / 1e18 * vprices[name] / 1e18 / 1e6
+            gauge_balance = gauge_balances[name] / 1e18 * vprices[name] / 1e18 / 1e6
+            s += supply
+            g += gauge_balance
+
+        times.append(time)
+        all_supplies.append(s)
+        all_gauge_balances.append(g)
+
+    plt.plot(times, all_supplies, c="gray", label="Total supply")
+    plt.plot(times, all_gauge_balances, c="blue", label="Staked supply")
+    plt.title("crvUSD main stablecoin pools supply")
+    plt.legend(loc="upper left")
+    plt.tick_params("x", rotation=45)
+    plt.ylabel("Supply (millions USD)")
+
+    plt.tight_layout()
+    plt.show()
---
+++ scripts/plot_yb_pools_imbalance_sum.py
@@ -0,0 +1,60 @@
+import pylab
+import numpy as np
+from brownie import multicall
+from collections import defaultdict
+from datetime import datetime
+from brownie import Contract, config
+from brownie import web3
+
+config['autofetch_sources'] = True
+
+FACTORY = "0x370a449FeBb9411c95bf897021377fe0B7D100c0"
+START_BLOCK = 23434125
+N_POINTS = 1000
+
+
+def main():
+    mc = Contract("0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696")
+
+    factory = Contract(FACTORY)
+    n = 3
+    markets = [factory.markets(i) for i in range(n)]
+    lts = [Contract(m[3]) for m in markets]
+    amms = [Contract(m[2]) for m in markets]
+    cryptopools = [Contract(amm.COLLATERAL()) for amm in amms]
+    collateral_decimals = [Contract(lt.ASSET_TOKEN()).decimals() for lt in lts]
+
+    current_block = web3.eth.block_number
+    blocks = [int(b) for b in np.linspace(START_BLOCK, current_block, N_POINTS)]
+
+    times = []
+    imbalances = defaultdict(list)
+    # debts = defaultdict(list)
+
+    for b in blocks:
+        params = defaultdict(dict)
+        with multicall(address=mc.address, block_identifier=b):
+            t = mc.getCurrentBlockTimestamp()
+            for i in range(n):
+                params[i]['b0'] = cryptopools[i].balances(0)
+                params[i]['b1'] = cryptopools[i].balances(1)
+                params[i]['p_o'] = cryptopools[i].price_oracle()
+                # params[i]['amm_debt'] = amms[i].get_debt()
+
+        t = datetime.fromtimestamp(t)
+        print(t)
+        times.append(t)
+
+        for i in range(n):
+            pool_value = params[i]['b1'] * 10**(18 - collateral_decimals[i]) * params[i]['p_o'] / 1e18 + params[i]['b0']
+            imbalance = params[i]['b0'] / pool_value
+            imbalances[i].append(imbalance)
+            # debts[i].append(params[i]['amm_debt'] / pool_value)
+
+    pylab.plot(times, np.array(list(imbalances.values()) * 100).mean(axis=0))
+
+    pylab.title('YB cryptopools imbalance in volatility')
+    pylab.ylabel('Fraction of pools in crvUSD [%]')
+    pylab.xticks(rotation=45, ha='right')
+    pylab.tight_layout()
+    pylab.show()
